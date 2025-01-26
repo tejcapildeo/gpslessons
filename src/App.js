99a69b06232f9a9493ee66284f9a044022dffbb1
tejcapildeo <tjkool500@gmail.com>
2025-01-26 03:40:11 -0500

    tried to fix build errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as  Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
